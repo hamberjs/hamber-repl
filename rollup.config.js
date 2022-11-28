e123c32ba0638456663d28efc8acc20eccad2c7f
NKDuy <kn145660@gmail.com>
2022-11-28 08:18:09 +0700

    pull CSS from hamberjs/template

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,6 @@
-import fs from 'fs';
 import hamber from 'rollup-plugin-hamber';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-
-// this is a bit of a hack
-const srcdoc = fs.readFileSync('src/Output/srcdoc.html', 'utf-8');
-fs.writeFileSync('src/Output/srcdoc.js', `export default ${JSON.stringify(srcdoc)};`);
 
 export default [
 	// tests
